a6f0530db62af61efe28dd0edc892c9beaae2bfd
Anthony Di Taranto <ajdt703@gmail.com>
2022-04-30 18:31:59 -0400

    Remove duplicate gitignore

---
+++ 03-MatrixTransformations/transform/target/debug/deps/transform-b9405e6d3e8f27ab.d
@@ -0,0 +1,5 @@
+d:\dev\git\graphics\glium-projects\03-MatrixTransformations\transform\target\debug\deps\transform-b9405e6d3e8f27ab.rmeta: src\main.rs
+
+d:\dev\git\graphics\glium-projects\03-MatrixTransformations\transform\target\debug\deps\transform-b9405e6d3e8f27ab.d: src\main.rs
+
+src\main.rs:
---
+++ 03-MatrixTransformations/transform/target/debug/deps/transform-bbfe5777035bc42b.d
@@ -0,0 +1,5 @@
+d:\dev\git\graphics\glium-projects\03-MatrixTransformations\transform\target\debug\deps\transform-bbfe5777035bc42b.rmeta: src\main.rs
+
+d:\dev\git\graphics\glium-projects\03-MatrixTransformations\transform\target\debug\deps\transform-bbfe5777035bc42b.d: src\main.rs
+
+src\main.rs:
