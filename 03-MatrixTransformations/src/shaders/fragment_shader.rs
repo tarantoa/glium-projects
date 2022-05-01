7b6e29cae084d50b115c9170ce2589307046b089
Anthony Di Taranto <ajdt703@gmail.com>
2022-04-30 21:37:17 -0400

    Add matrix transformation demo

---
+++ 03-MatrixTransformations/src/shaders/fragment_shader.rs
@@ -0,0 +1,9 @@
+pub const FRAGMENT_SHADER_SRC: &str = r#"
+    #version 140
+
+    out vec4 color;
+
+    void main() {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+"#;
---
+++ 03-MatrixTransformations/src/shaders/vertex_shader.rs
@@ -0,0 +1,11 @@
+pub const VERTEX_SHADER_SRC: &str = r#"
+    #version 140
+
+    in vec2 position;
+
+    uniform mat4 matrix;
+
+    void main() {
+        gl_Position = matrix * vec4(position, 0.0, 1.0);
+    }
+"#;
